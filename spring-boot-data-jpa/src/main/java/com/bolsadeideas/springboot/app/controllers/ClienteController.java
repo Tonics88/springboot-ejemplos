647c71a983971f98360e179e73cc374baecf64d5
Tonics88 <tcs1988@gmail.com>
2018-08-20 15:58:53 +0200

    Añadido editar Cliente

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -7,13 +7,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 
     @Autowired
@@ -39,13 +43,31 @@ public class ClienteController {
         return "form";
     }
 
+    @RequestMapping(value = "/form/{id}")
+    public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+
+        Cliente cliente = null;
+
+        if (id>0) {
+            cliente = clienteDao.findOne(id);
+        } else {
+            return "redirect:/listar";
+        }
+
+        model.put("cliente", cliente);
+        model.put("titulo", "Editar Cliente");
+
+        return "form";
+    }
+
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
         if (result.hasErrors()) {
             model.addAttribute("titulo", "Formulario de Cliente");
             return "form";
         }
         clienteDao.save(cliente);
+        status.setComplete();
         return "redirect:listar";
     }
 
