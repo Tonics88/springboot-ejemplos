b4b7c2a1b99b12e9492c1ae6e9d680c58913c194
Tonics88 <tcs1988@gmail.com>
2018-08-20 12:12:46 +0200

    Creada Vista "listar" para Cliente

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, ' Andres', 'Guzman', 'profesor@bolsadeideas.com', '2018-08-20');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, ' John', 'Doe', 'john.doe@gmail.com', '2018-08-20');
